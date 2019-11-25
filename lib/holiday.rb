d86cf979df9ac95b27ce1ff3e12cf1fe2e6421ed
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2019-11-25 23:35:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -7,8 +7,8 @@ def second_supply_for_fourth_of_july(holiday_hash)
    },
      :summer => {
        :fourth_of_july => {
-         supplies: 
-       } 
+         supplies: "Fireworks", "BBQ"
+       }
         
    },
      :fall => {
